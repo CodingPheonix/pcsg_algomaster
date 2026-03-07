de64848e0eebe139cd7feaefebde4f90a0cf47ea
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2026-03-07 21:25:43 +0530

    applied light mode to algorithms page

--- app/algorithms/page.tsx
+++ app/algorithms/page.tsx
@@ -5,16 +5,16 @@ import AlgoCard from '../components/AlgoCard'
 
 const page = () => {
     return (
-        <div className='bg-black'>
+        <div className='bg-white text-black'>
             <Navbar />
 
             <div className='pt-20 w-[75%] mx-auto'>
                 <div className='flex justify-between items-baseline mb-5 mt-10'>
-                    <h1 className="text-3xl font-bold text-white">Algorithm</h1>
-                    <p className="text-gray-300 text-sm">Last Updated: 2026-02-24</p>
+                    <h1 className="text-3xl font-bold text-blue-500">Algorithm</h1>
+                    <p className="text-gray-400 text-sm">Last Updated: 2026-02-24</p>
                 </div>
 
-                <div className='w-full mx-auto my-2 h-auto bg-black text-gray-400 p-3 rounded-t-lg font-mono text-start border border-green-500 rounded-xl'>
+                <div className='w-full mx-auto my-2 h-auto bg-white text-gray-400 p-3 rounded-t-lg font-mono text-start border border-blue-500 rounded-xl'>
                     <div className='w-[90%] mx-auto flex'>
                         <p className="w-1/4">Heading</p>
                         <p className="w-1/4">Description</p>
@@ -25,11 +25,11 @@ const page = () => {
 
                 {content.map((section, index) => (
                     <div key={index} className="border border-gray-800 ">
-                        <h2 className="text-xl font-bold w-full bg-green-500 text-black p-2">#  {section.heading}</h2>
+                        <h2 className="text-xl font-bold w-full bg-blue-500 text-white p-2">#  {section.heading}</h2>
                         {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4"> */}
                         <div className="flex flex-col mt-4 mb-3 w-[90%] mx-auto gap-4">
                             {section.course.map((item, index) => (
-                                <div key={index} className="bg-gray-900 p-4 rounded-lg border border-gray-700 flex justify-around items-center hover:cursor-pointer">
+                                <div key={index} className="text-black p-4 rounded-lg border border-gray-700 flex justify-around items-center hover:cursor-pointer">
                                     <AlgoCard {...item} />
                                 </div>
                             ))}
--- app/components/AlgoCard.tsx
+++ app/components/AlgoCard.tsx
@@ -23,10 +23,10 @@ const AlgoCard = (item: CourseItem) => {
 
     return (
         <div className='flex w-full justify-around items-center'>
-            <h3 className="text-lg font-semibold text-white w-1/4 text-start">{item.name}</h3>
-            <p className="text-sm text-gray-300 mt-2 w-1/4 text-start">{item.description}</p>
+            <h3 className="text-lg font-semibold w-1/4 text-start">{item.name}</h3>
+            <p className="text-sm mt-2 w-1/4 text-start">{item.description}</p>
             <span className='w-1/4 flex justify-center items-center'>
-                <span className={`inline-block px-2 py-1 text-md rounded-full mt-2 ${item.difficulty === "Easy" ? "text-green-500" :
+                <span className={`inline-block px-2 py-1 text-md rounded-full font-semibold mt-2 ${item.difficulty === "Easy" ? "text-green-500" :
                     item.difficulty === "Medium" ? "text-yellow-500 " :
                         "text-red-500"
                     }`}>
@@ -34,13 +34,13 @@ const AlgoCard = (item: CourseItem) => {
                 </span>
             </span>
             <span className='flex w-1/4 justify-center items-center gap-3'>
-                <button className="hover:bg-gray-800 text-white hover:text-blue-600 px-3 py-1 rounded mr-2">
+                <button className=" text-black hover:text-blue-600 px-3 py-1 rounded mr-2">
                     <FileSpreadsheet className="w-4 h-4" />
                 </button>
-                <button onClick={() => {router.push(`/algorithms/tutorial?id=${item.id}`)}} className="hover:bg-gray-800 text-white hover:text-yellow-500 px-3 py-2 rounded">
+                <button onClick={() => {router.push(`/algorithms/tutorial?id=${item.id}`)}} className=" text-black hover:text-yellow-500 px-3 py-2 rounded">
                     <Play className="w-4 h-4" />
                 </button>
-                <button className="hover:bg-gray-800 text-white hover:text-red-500 px-3 py-2 rounded">
+                <button className=" text-black hover:text-red-500 px-3 py-2 rounded">
                     <Youtube className="w-4 h-4" />
                 </button>
             </span>
