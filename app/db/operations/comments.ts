d7397f0464d572ba8060b42b4e5263e30b2449c4
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2026-03-27 21:39:57 +0530

    updated comments table

--- app/db/operations/comments.ts
+++ app/db/operations/comments.ts
@@ -3,17 +3,28 @@
 import { eq } from "drizzle-orm"
 import { db } from ".."
 import { commentsTable } from "../schema"
+import { prisma } from "../prisma"
 
-export const uploadComment = async ({id, topic_id, username, message, time} : {id: string, topic_id: string, username: string, message: string, time: Date}) => {
+export const uploadComment = async ({ id, topic_id, username, message, time }: { id: string, topic_id: string, username: string, message: string, time: Date }) => {
     try {
-        await db
-        .insert(commentsTable)
-        .values({
-            id,
-            topic_id,
-            username,
-            message,
-            time
+        // await db
+        // .insert(commentsTable)
+        // .values({
+        //     id,
+        //     topic_id,
+        //     username,
+        //     message,
+        //     time
+        // })
+
+        await prisma.comments_table.create({
+            data: {
+                id,
+                topic_id,
+                username,
+                message,
+                time
+            }
         })
     } catch (error) {
         throw error;
@@ -22,10 +33,16 @@ export const uploadComment = async ({id, topic_id, username, message, time} : {i
 
 export const fetchComments = async (id: string) => {
     try {
-        return await db
-        .select()
-        .from(commentsTable)
-        .where(eq(commentsTable.topic_id, id))
+        // return await db
+        //     .select()
+        //     .from(commentsTable)
+        //     .where(eq(commentsTable.topic_id, id))
+
+        return await prisma.comments_table.findMany({
+            where: {
+                topic_id: id
+            }
+        })
     } catch (error) {
         throw error;
     }
