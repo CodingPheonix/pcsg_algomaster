3407b258b851c757e6c7bdac6fbe0a90a7a6da6c
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2026-03-27 22:27:00 +0530

    subtopic table

--- app/db/operations/subtopics.ts
+++ app/db/operations/subtopics.ts
@@ -3,18 +3,30 @@
 import { eq } from "drizzle-orm"
 import { db } from ".."
 import { SubTopic, subtopicTable } from "../schema"
+import { prisma } from "../prisma"
 
-export const addSubTopic = async (subTopic: SubTopic) => {
+export const addSubTopic = async (subTopic: SubTopic, tutorialId: string) => {
     try {
-        await db
-            .insert(subtopicTable)
-            .values({
+        // await db
+        //     .insert(subtopicTable)
+        //     .values({
+        //         id: subTopic.id,
+        //         name: subTopic.name,
+        //         description: subTopic.description,
+        //         difficulty: subTopic.difficulty,
+        //         external_video: subTopic.external_video
+        //     })
+
+        await prisma.subtopic_table.create({
+            data: {
                 id: subTopic.id,
                 name: subTopic.name,
                 description: subTopic.description,
                 difficulty: subTopic.difficulty,
-                external_video: subTopic.external_video
-            })
+                external_video: subTopic.external_video,
+                tutorial_id: tutorialId
+            }
+        })
 
     } catch (error) {
         console.error(error)
@@ -23,15 +35,27 @@ export const addSubTopic = async (subTopic: SubTopic) => {
 
 export const editSubTopic = async (subTopic: SubTopic) => {
     try {
-        await db
-            .update(subtopicTable)
-            .set({
+        // await db
+        //     .update(subtopicTable)
+        //     .set({
+        //         name: subTopic.name,
+        //         description: subTopic.description,
+        //         difficulty: subTopic.difficulty,
+        //         external_video: subTopic.external_video
+        //     })
+        //     .where(eq(subtopicTable.id, subTopic.id))
+
+        await prisma.subtopic_table.update({
+            where: {
+                id: subTopic.id
+            },
+            data: {
                 name: subTopic.name,
                 description: subTopic.description,
                 difficulty: subTopic.difficulty,
                 external_video: subTopic.external_video
-            })
-            .where(eq(subtopicTable.id, subTopic.id))
+            }
+        })
 
     } catch (error) {
         console.error(error)
@@ -40,9 +64,15 @@ export const editSubTopic = async (subTopic: SubTopic) => {
 
 export const remove_Subtopic = async (subtopicId: string) => {
     try {
-        await db
-            .delete(subtopicTable)
-            .where(eq(subtopicTable.id, subtopicId))
+        // await db
+        //     .delete(subtopicTable)
+        //     .where(eq(subtopicTable.id, subtopicId))
+
+        await prisma.subtopic_table.delete({
+            where: {
+                id: subtopicId
+            }
+        })
     } catch (error) {
         console.error(error)
     }
