cdb8be93dc26836ea3239ac320b88b276ecc8ecb
Sayan Biswas <168727255+CodingPheonix@users.noreply.github.com>
2026-03-28 22:16:24 +0530

    deployment fix

--- app/db/prisma.ts
+++ app/db/prisma.ts
@@ -1,12 +1,35 @@
+// import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+// import { PrismaClient } from "@/generated/prisma/client";
+
+// const adapter = new PrismaMariaDb({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   port: Number(process.env.DATABASE_PORT),
+// });
+// const prisma = new PrismaClient({ adapter });
+// export { prisma };
+
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const globalForPrisma = globalThis as any;
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
+  port: Number(process.env.DATABASE_PORT),
+  connectionLimit: 10,
+  acquireTimeout: 30000,
 });
-const prisma = new PrismaClient({ adapter });
-export { prisma };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
